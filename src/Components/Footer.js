ca109a8b4ffd3d23293faf6940f67d675e1c8b8d
Luiz Guilherme <guilhermerx18@gmail.com>
2022-05-11 11:21:55 -0300

    corrigido posicionamento dos elementos no footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer>
           <div className="endereco">
-            <p>Endereço</p>
+            <p>Endereço:</p>
             <div>
               <p>Rua Oitenta - 216</p>
               <p>Rústico, Volta Redonda-Rj</p>
@@ -14,14 +14,14 @@ export default function Footer() {
           </div>
 
           <div className="telefone">
-            <p>Telefone</p>
+            <p>Telefone:</p>
             <div>
               <p><FaWhatsappSquare />(24) 99936-2399</p>
             </div>
           </div>
 
           <div className="contatos">
-            <p>Redes Sociais</p>
+            <p>Redes Sociais:</p>
             <div>
               <a href='https://www.instagram.com/a_casa_da_mae_joana_doceria/' target='_blank' rel="noreferrer"><AiFillInstagram /></a>
               <a href='https://www.facebook.com/Soniaaa.costa01' target='_blank' rel="noreferrer"><AiFillFacebook /></a>
